d28cf6a5bebb5d07b522f482c7e441d0955ccaa9
Julian Benda <julian.benda@ovgu.de>
2021-04-08 12:21:09 +0200

    Update hello-world example
    from: Bella42/julea/improve-example-docu

--- example/hello-world.c
+++ example/hello-world.c
@@ -1,6 +1,6 @@
 /*
  * JULEA - Flexible storage framework
- * Copyright (C) 2019-2021 Michael Kuhn
+ * Copyright (C) 2019-2020 Michael Kuhn
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -24,78 +24,106 @@
 #include <locale.h>
 #include <stdio.h>
 
+
+//TODO: this example currently does not check whether this object already exists
+int object_store_write()
+{
+	//g_autoptr declares a pointer variable with automatic cleanup
+	g_autoptr(JBatch) batch = NULL; 		// a batch in which the operations are executed
+	g_autoptr(JObject) object = NULL;		// an object to be stored
+
+	gboolean is_executed = false;			// batch status
+	guint64 bytes_written = 0;				// number of bytes actually written
+	gchar const* data = "Hello World!"; 	// our example object data is this string
+	gchar const* name_space = "object-namespace";
+	gchar const* name = "object-1";
+	
+	// in C a string needs to be terminated by a null character '\0' which needs space.
+	// therefore the data_size is the length of the string (strlen) plus 1.
+	guint64 data_size = strlen(data) + 1;	// size of the object
+
+	// Explicitly enable UTF-8 since functions such as g_format_size might return UTF-8 characters.
+	setlocale(LC_ALL, "C.UTF-8");
+
+	// creates a new batch with the default semantics
+	batch = j_batch_new_for_template(J_SEMANTICS_TEMPLATE_DEFAULT);
+
+	// returns a new object handler
+	object = j_object_new(name_space, name);
+	
+	//creates the actual object
+	j_object_create(object, batch);
+
+	// Writes data for object. Afterwards bytes_written contains the 
+	// number of bytes that were actually written
+	j_object_write(object, data, data_size, 0, &bytes_written, batch);
+	
+	// Executes the object creation and the object write in one batch.
+	// returns false if an error occurred 
+	is_executed = j_batch_execute(batch);
+
+	if (!is_executed)
+	{
+		printf("An error occurred when writing the object! \n");
+	}
+	else
+	{
+		// check whether everything was written as this might not be the case
+		if (bytes_written == data_size)
+		{
+			printf("Writing was successful :-) \n");
+		}
+		else
+		{
+			printf("Only %ld bytes written instead of %ld\n", bytes_written, data_size);
+		}
+	}
+
+	return 0;
+}
+
+int object_store_read()
+{
+	return 0;
+}
+
+int key_value_store_write()
+{
+	return 0;
+}
+
+int key_value_store_read()
+{
+	return 0;
+}
+
+int database_write()
+{
+	return 0;
+}
+
+int database_read()
+{
+	return 0;
+}
+
 int
 main(int argc, char** argv)
 {
-	g_autoptr(JBatch) batch = NULL;
-	g_autoptr(JDBEntry) entry = NULL;
-	g_autoptr(JDBSchema) schema = NULL;
-	g_autoptr(JKV) kv = NULL;
-	g_autoptr(JObject) object = NULL;
-
-	gchar const* hello_world = "Hello World!";
-	guint64 nbytes;
-
 	(void)argc;
 	(void)argv;
 
 	// Explicitly enable UTF-8 since functions such as g_format_size might return UTF-8 characters.
 	setlocale(LC_ALL, "C.UTF-8");
 
-	// FIXME: this example does not clean up after itself and will only work if the object, kv pair and db entry do not exist already
-	// FIXME: add more error checking
+	object_store_write();
+	object_store_read();
 
-	batch = j_batch_new_for_template(J_SEMANTICS_TEMPLATE_DEFAULT);
-	object = j_object_new("hello", "world");
-	kv = j_kv_new("hello", "world");
-	schema = j_db_schema_new("hello", "world", NULL);
-	j_db_schema_add_field(schema, "hello", J_DB_TYPE_STRING, NULL);
-	entry = j_db_entry_new(schema, NULL);
-	j_db_entry_set_field(entry, "hello", hello_world, strlen(hello_world) + 1, NULL);
+	key_value_store_write();
+	key_value_store_read();
 
-	j_object_create(object, batch);
-	j_object_write(object, hello_world, strlen(hello_world) + 1, 0, &nbytes, batch);
-	j_kv_put(kv, g_strdup(hello_world), strlen(hello_world) + 1, g_free, batch);
-	j_db_schema_create(schema, batch, NULL);
-	j_db_entry_insert(entry, batch, NULL);
-
-	if (j_batch_execute(batch))
-	{
-		g_autoptr(JDBIterator) iterator = NULL;
-		g_autoptr(JDBSelector) selector = NULL;
-
-		gchar buffer[128] = { '\0' };
-		g_autofree gpointer value = NULL;
-		guint32 length = 0;
-		g_autofree gchar* db_field = NULL;
-		guint64 db_length = 0;
-
-		j_object_read(object, buffer, 128, 0, &nbytes, batch);
-
-		if (j_batch_execute(batch))
-		{
-			printf("Object contains: %s (%" G_GUINT64_FORMAT " bytes)\n", buffer, nbytes);
-		}
-
-		j_kv_get(kv, &value, &length, batch);
-
-		if (j_batch_execute(batch))
-		{
-			printf("KV contains: %s (%" G_GUINT32_FORMAT " bytes)\n", (gchar*)value, length);
-		}
-
-		selector = j_db_selector_new(schema, J_DB_SELECTOR_MODE_AND, NULL);
-		j_db_selector_add_field(selector, "hello", J_DB_SELECTOR_OPERATOR_EQ, hello_world, strlen(hello_world) + 1, NULL);
-		iterator = j_db_iterator_new(schema, selector, NULL);
-
-		while (j_db_iterator_next(iterator, NULL))
-		{
-			JDBType type;
-
-			j_db_iterator_get_field(iterator, "hello", &type, (gpointer*)&db_field, &db_length, NULL);
-			printf("DB contains: %s (%" G_GUINT64_FORMAT " bytes)\n", db_field, db_length);
-		}
-	}
+	database_write();
+	database_read();
 
 	return 0;
 }
